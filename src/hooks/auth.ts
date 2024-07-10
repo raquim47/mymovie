747631ab12266f1cb7c6d0707f57a16639d19721
[SW5] 홍승진 <cmikal47@gmail.com>
2024-07-10 17:27:05 +0900

    Refactor : auth 커스텀 훅 수정 -> 공통부분 useAuthMutation으로 분리

--- src/hooks/auth.ts
+++ src/hooks/auth.ts
@@ -1,81 +1,50 @@
 import { useDispatch } from 'react-redux';
-import { useMutation, useQuery } from 'react-query';
+import { MutationFunction, useMutation, useQuery } from 'react-query';
 import { googleLogin, initAuth, login, logout, signUp } from 'services/auth';
 import { setUserState } from 'store/user';
-import { ILoginCredentials, ISignUpCredentials } from 'services/auth/types';
 import { useNavigate } from 'react-router-dom';
 import { setErrors } from 'store/auth-form';
 import { queryClient } from 'index';
 
-export const useAuthSuccessHandler = () => {
+const useAuthMutation = <T = void>(mutationFn: MutationFunction<void, T>) => {
   const navigate = useNavigate();
-
-  return () => {
-    queryClient.invalidateQueries('initAuth');
-    navigate('/');
-  };
-};
-
-export const useAuthErrorHandler = () => {
   const dispatch = useDispatch();
 
-  return (error: unknown) => {
-    if (error instanceof Error) {
-      const parsedError = JSON.parse(error.message);
-      if (parsedError.name === 'popup-close') {
-        return;
-      }
+  return useMutation(mutationFn, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('initAuth');
+      navigate('/');
+    },
+    onError: (error: unknown) => {
+      if (error instanceof Error) {
+        const parsedError = JSON.parse(error.message);
+        if (parsedError.name === 'popup-close') {
+          return;
+        }
 
-      if (parsedError.name === 'global') {
-        alert(parsedError.message);
+        if (parsedError.name === 'global') {
+          alert(parsedError.message);
+        } else {
+          dispatch(setErrors({ [parsedError.name]: parsedError.message }));
+        }
       } else {
-        dispatch(setErrors({ [parsedError.name]: parsedError.message }));
+        alert('요청이 실패했습니다. 다시 시도해주세요.');
       }
-    } else {
-      alert('요청이 실패했습니다. 다시 시도해주세요.');
-    }
-  };
-};
-
-// 회원가입
-export const useSignUp = () => {
-  const handleSuccess = useAuthSuccessHandler();
-  const handleError = useAuthErrorHandler();
-
-  return useMutation((credential: ISignUpCredentials) => signUp(credential), {
-    onSuccess: handleSuccess,
-    onError: handleError,
+    },
   });
 };
 
 // 로그인
-export const useLogin = () => {
-  const handleSuccess = useAuthSuccessHandler();
-  const handleError = useAuthErrorHandler();
+export const useLogin = () => useAuthMutation(login);
 
-  return useMutation((credential: ILoginCredentials) => login(credential), {
-    onSuccess: handleSuccess,
-    onError: handleError,
-  });
-};
+// 구글 로그인
+export const useGoogleLogin = () => useAuthMutation(googleLogin);
 
-export const useGoogleLogin = () => {
-  const handleSuccess = useAuthSuccessHandler();
-  const handleError = useAuthErrorHandler();
-
-  return useMutation(googleLogin, {
-    onSuccess: handleSuccess,
-    onError: handleError,
-  });
-};
+// 회원가입
+export const useSignUp = () => useAuthMutation(signUp);
 
 // 로그아웃
-export const useLogout = () => {
-  const handleSuccess = useAuthSuccessHandler();
-  return useMutation(logout, {
-    onSuccess: handleSuccess,
-  });
-};
+export const useLogout = () => useAuthMutation(logout);
 
 // 인증 상태 확인
 export const useInitAuth = () => {
