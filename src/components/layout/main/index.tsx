7f7d4d4e28b14dce38d707a6812ec245bea0f9b2
[SW5] 홍승진 <cmikal47@gmail.com>
2024-08-15 01:45:42 +0900

    Feat : 페이지 이동 시 맨 위로 스크롤, /movies 제외

--- src/components/layout/main/index.tsx
+++ src/components/layout/main/index.tsx
@@ -1,8 +1,15 @@
 import Header from 'components/header';
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import * as S from './styles';
 
 const MainLayout = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    if (!pathname.startsWith('/movies')) {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname]);
   return (
     <S.Container>
       <Header />
